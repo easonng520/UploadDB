a9638a16a9d9461468c3d5410e1a29d822362249
easonng520 <121944894+easonng520@users.noreply.github.com>
2023-05-24 11:28:16 +0000

    Initial commit

---
+++ src/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "john.db.elephantsql.com",
+  port: 5432,
+  USER: "nikqpylt",
+  PASSWORD: "nOkSqQJ6M9zwOMyVwX5jiCvDIPT6dN9p",
+  DB: "nikqpylt",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
